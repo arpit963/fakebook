23cf18f12c1d31bb6336c5ba8cae260f4ee76667
arpit963 <arpit3072@gmail.com>
2021-06-01 16:26:43 +0530

    django_heroku

--- fakebook/settings.py
+++ fakebook/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -185,3 +185,6 @@ MEDIA_ROOT = BASE_DIR / 'static_cdn' / 'media_root'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# activate Django-heroku
+django_heroku.settings(locals())
